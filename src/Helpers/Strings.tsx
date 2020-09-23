6141a8d6366e8590c7b4da48f4004576bcbacc35
Narendra Santhosh Nagarajan <santhoshnarendra@gmail.com>
2020-09-23 18:19:19 +0530

    Marker-added

--- src/Helpers/Strings.tsx
+++ src/Helpers/Strings.tsx
@@ -2,29 +2,66 @@ export const SLIDE_ITEMS = [
   {
     label: 'Connect',
     right: false,
-    color:'#BFEFA5',
+    color: '#BFEFA5',
     header: 'Over 200+ Qualified Tutors',
-    headerCaption: 'Tutors are verified and best ones are onboarded from across the Country',
+    headerCaption:
+      'Tutors are verified and best ones are onboarded from across the Country',
   },
   {
     label: 'Learn',
     right: true,
-    color:'#bfeaf5',
+    color: '#bfeaf5',
     header: 'Book a Slot and Start Learning',
     headerCaption: 'CHoose a Tutor and available slot to start the learning',
   },
   {
     label: 'Interact',
     right: false,
-    color:'#ffe4d9',
+    color: '#ffe4d9',
     header: 'Video Calls with presentation',
-    headerCaption: 'Students can interact and keep track of their Doubts and notes',
+    headerCaption:
+      'Students can interact and keep track of their Doubts and notes',
   },
   {
     label: 'Excel',
     right: true,
-    color:'#ffdddd',
+    color: '#ffdddd',
     header: 'One Platform for all your Learning',
     headerCaption: 'Digital Experience with traditional flavours',
+  },
+];
+
+export const TUTOR_CORDINATES = [
+  {
+    label: 'Rick Harrison',
+    image: require('../assets/images/tutor/tutor-1.jpeg'),
+    coordinate: {
+      latitude: 37.8025259,
+      longitude: -122.4351431,
+    },
+  },
+  {
+    label: 'Chumlee',
+    image: require('../assets/images/tutor/Tutor-2.jpg'),
+    coordinate: {
+      latitude: 37.7896386,
+      longitude: -122.421646,
+    },
+  },
+  {
+    label: 'Bagit',
+    image: require('../assets/images/tutor/Tutor-3.jpeg'),
+    coordinate: {
+      latitude: 37.7665248,
+      longitude: -122.4161628,
+    },
+  },
+  {
+    label: 'Marry Thomas',
+    image: require('../assets/images/tutor/Tutor-4.jpeg'),
+    coordinate: {
+      latitude: 37.7734153,
+      longitude: -122.4577787,
+    },
   },
 ];
--- src/Screens/Maps/Maps.tsx
+++ src/Screens/Maps/Maps.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {TUTOR_CORDINATES} from '../../Helpers/Strings';
+import {View, StyleSheet, Image} from 'react-native';
+import {Avatar} from 'react-native-elements';
+import {Box, Text} from '../../Config/theme';
+import MapView, {PROVIDER_GOOGLE, Marker, Callout} from 'react-native-maps';
 const styles = StyleSheet.create({
   maps: {
     height: '100%',
   },
 });
+
 const Maps = () => {
   return (
     <MapView
@@ -16,8 +20,32 @@ const Maps = () => {
         longitude: -122.4324,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
-      }}
-    />
+      }}>
+      {TUTOR_CORDINATES.map((tutor, index) => {
+        return (
+          <Marker key={index} title={tutor.label} coordinate={tutor.coordinate}>
+            <Callout>
+              <Box
+                flex={1}
+                width={200}
+                alignItems="center"
+                justifyContent="center"
+                flexDirection="row">
+                <Avatar size="large" rounded source={tutor.image} />
+                <Box>
+                  <Text variant="heading4">{tutor.label}</Text>
+                  <Text variant="heading6">Physics</Text>
+                </Box>
+              </Box>
+            </Callout>
+            <Avatar
+              size="small"
+              source={require('../../assets/images/general/logo.png')}
+            />
+          </Marker>
+        );
+      })}
+    </MapView>
   );
 };
 
